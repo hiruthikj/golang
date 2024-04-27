2a7f7632e32ad200c9d6a8de7480af089a7eb158
Hiruthik J <hiruthik27@gmail.com>
2024-04-27 15:54:05 +0530

    chore

--- learning/myapp/server.go
+++ learning/myapp/server.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
+
+type User struct {
+	Name string `json:"name" form:"name" query:"name"`
+	Email string `json:"email" form:"email" query:"name"`
+}
+
+type UserDTO struct {
+	Name string
+	Email string
+	IsAdmin bool
+}
 
 func main() {
 	e := echo.New()
@@ -13,5 +25,25 @@ func main() {
 		return ctx.String(http.StatusOK, ctx.QueryParam("id"))
 	})
 
-	e.Logger.Warn(e.Start(":3001"))
+	e.POST("/users", func(c echo.Context) error {
+		u := &User{}
+		err := c.Bind(u); if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		user := UserDTO{
+			Name: u.Name,
+			Email: u.Email,
+			IsAdmin: false,
+		}
+
+		c.Logger().Infof("Created %v", user)
+
+		return c.String(http.StatusCreated, "Created User")
+	})
+
+	if 	err:= e.Start(":1323"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
 }

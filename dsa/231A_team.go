2d36d09c76bc4ba4efff4b394e51ee596d6a5749
Hiruthik J <hiruthik27@gmail.com>
2024-06-26 16:21:49 +0530

    chore

---
+++ dsa/231A_team.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  var problems int
+  fmt.Scanf("%d\n", &problems)
+  
+  var a,b,c int
+  result := 0
+  for i:=0; i<problems; i++ {
+	fmt.Scanf("%d %d %d\n", &a, &b, &c)
+
+	result += a&b | b&c | c&a
+  }
+  fmt.Println(result)
+}
